a37b9cfb42cad8817a90de4cf3c5ef375dfd2781
Mohammad Mohsen Haidari <mohsenhaidari0766@gamil.com>
2024-01-10 14:50:34 +0430

    article controller methods added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :set_turbo_frame
+    helper_method :current_user 
   
     protected
   
@@ -8,7 +9,7 @@ class ApplicationController < ActionController::Base
     end
   
     def current_user
-      @current_user ||= User.find_or_create_by(ip_address: request.remote_ip)
+      @current_user ||= User.find_or_create_by(user_ip: request.remote_ip)
     end
   end
   
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,45 @@
-class ArticlesController < ApplicationController  
+class ArticlesController < ApplicationController
     def index
+        if query_param.present? 
+          search_create if  query_param.length >= 3
+      
+          @articles = Article.search_title(query_param).limit(50)
+        else
+          @articles = Article.limit(9)
+        end
+      end
+      
+
+  private
+
+  def search_create
+    if latest_search.nil? || !matched_articles(latest_search.query, query_param)
+      Search.create(query: query_param.strip, user: current_user)
+    else
+      latest_search.update(query: query_param)
     end
+  end
+
+  def latest_search
+    @latest_search ||= Search.latest_search(current_user)
+  end
+
+  def query_param
+    params[:query]&.strip
+  end
+
+  def create_new_search_record
+    Search.create(query: query_param, user: current_user, ip_address: current_user.ip_address)
+  end
+
+  
+  def update_existing_search_record
+    latest_search.update(query: query_param)
+  end
+
+  def matched_articles(str1, str2)
+    winkler = Amatch::JaroWinkler.new(str1)
+    similarity_distance = winkler.match(str2)
+    similarity_distance > 0.7
+  end
 end
