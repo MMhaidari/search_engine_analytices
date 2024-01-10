195f101aa973d33201d78c0b169c69bb7bb0e950
Mohammad Mohsen Haidari <mohsenhaidari0766@gamil.com>
2024-01-10 11:26:37 +0430

    controllers files and views files added and also current user created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
-end
+    before_action :set_turbo_frame
+  
+    protected
+  
+    def set_turbo_frame
+      request.variant = :turbo_frame if turbo_frame_request?
+    end
+  
+    def current_user
+      @current_user ||= User.find_or_create_by(ip_address: request.remote_ip)
+    end
+  end
+  
--- app/models/search.rb
+++ app/models/search.rb
@@ -2,4 +2,10 @@ class Search < ApplicationRecord
     belongs_to :user
 
     validates :query, presence: true, length: { minimum: 5, maximum: 100 }
+
+    scope :sorted_by_search_count_descending, -> {
+        group(:query)
+          .select('query, COUNT(*) as total')
+          .order('total DESC')
+      }
 end
